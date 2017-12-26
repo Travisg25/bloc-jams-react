7b6faea31e4abe86ab090e65e19d38d818f3077c
Travis <Travisagrossman@gmail.com>
2017-12-26 15:40:34 -0800

    checkpoint 3 complete

---
+++ src/components/Landing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Landing = () => (
+  <section className= 'landing'>
+    Landing page Here
+    </section>
+);
+
+export default Landing;
---
+++ src/components/Library.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Library = () => (
+  <section className= 'library'>
+    Library page Here
+    </section>
+);
+
+export default Library;
