479e4382470848064255196782c37fc3c275b3d7
Travis <Travisagrossman@gmail.com>
2017-12-28 15:15:19 -0800

    assignment 6 complete song list added

--- src/components/Album.js
+++ src/components/Album.js
@@ -7,20 +7,16 @@ class Album extends Component {
     const album = albumData.find( album => {
       return album.slug === this.props.match.params.slug
     });
-
     this.state = {
-      album: album
+      album: album,
     };
   }
-
-
-
-
 
   render() {
     return (
       <section className= 'album'>
         <section id="album-info">
+
         <img id="album-cover-art" src={this.state.album.albumCover} />
           <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
@@ -28,15 +24,36 @@ class Album extends Component {
             <div id="release-info">{this.state.album.releaseInfo}</div>
           </div>
         </section>
+
         <table id="song-list">
           <colgroup>
             <col id="song-number-column" />
             <col id="song-title-column" />
             <col id="song-duration-column" />
           </colgroup>
+
           <tbody>
+            {
+              this.state.album.songs.map( (songs, index) =>
+              {
+                return(
+                  <tr key= {index}>
+                    <button >
+                      <span class = "song-number">{index +1} </span>
+                      <span class = "ion-play"> </span>
+                      <span class = "ion-pause" > </span>
+                    </button>
+                    <td>{index +1}</td>
+                    <td>{songs.title}</td>
+                    <td>{songs.duration}</td>
+                  </tr>
+                )}
+              )
+            }
           </tbody>
+
         </table>
+
      </section>
     );
   }
